60861d58597836c60bbf130a2d8e6be20fa5db9f
root <root@LAPTOP-I7MBNFB9>
2023-03-25 22:40:53 +0300

    .

---
+++ brain_games/gcd.py
@@ -0,0 +1,20 @@
+from random import randint
+
+
+START_GAME = 'Find the greatest common divisor of given numbers.'
+
+
+def get_gcd(num1, num2):
+    divisor = min(num1, num2)
+    while divisor > 0:
+        if num1 % divisor == 0 and num2 % divisor == 0:
+            return divisor
+        divisor -= 1
+
+
+def print_question():
+    num1 = randint(1, 50)
+    num2 = randint(1, 50)
+    question = f'{num1} {num2}'
+    answer = get_gcd(num1, num2)
+    return question, str(answer)
