8c67c6096460bb31c9a7ddc975a654a35efdcc37
root <root@LAPTOP-I7MBNFB9>
2023-03-25 20:32:42 +0300

    .

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,26 +1,12 @@
-#!/usr/bin/env/python
-
-import prompt
-from random import randint
+#!/usr/bin/env python3
+from brain_games.games import even
+from brain_games.core import process_game
 
 
 def main():
-    print("Welcome to the Brain Games!")
-    user_name = prompt.string("May I have your name? ")
-    print(f"Hello, {user_name}!\nAnswer 'yes' if the number is even, otherwise answer 'no'")
-    index = 0
-    winscore = 3
-    while index < winscore:
-        index += 1
-        random_number = randint(1, 100)
-        print(f"Question: {random_number}")
-        user_answer = prompt.string("Your answer: ")
-        if (user_answer == 'yes' and random_number % 2 == 0) or (user_answer == 'no' and random_number % 2 != 0):
-            print(f'Correct!')
-        elif user_answer == 'yes' and random_number % 2 != 0:
-            print(f"'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {user_name}!")
-            return
-        elif user_answer == 'no' and random_number % 2 == 0:
-            print(f"'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {user_name}!")
-            return
-    print(f"Congratulations, {user_name}!")
+    process_game(even)
+
+
+if __name__ == "__main__":
+    main()
+
