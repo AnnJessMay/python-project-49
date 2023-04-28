b8f1823974c6b8ec0f29e78b2e4bf692943f2894
root <root@LAPTOP-I7MBNFB9>
2023-04-28 17:32:37 +0300

    .

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
+from brain_games import cli
 
-def greet():
-    print('Welcome to the Brain Games!')
 
 def main():
-    greet()
+    print('Welcome to the Brain Games!')
+    cli.welcome_user()
 
 
 if __name__ == '__main__':
-    greet()
-
-from brain_games.cli import welcome_user
+    main()
