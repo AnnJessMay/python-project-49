a6a956eec77f75b747e35849425c78664488af08
root <root@LAPTOP-I7MBNFB9>
2023-05-02 17:42:48 +0300

    .

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-DESCRIPTION = 'Answer "yes" if given number is prime.Otherwise answer "no".'
+DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
