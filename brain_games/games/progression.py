aa09ded2a792037af27d54ed3a4815c3cb6af0ae
root <root@LAPTOP-I7MBNFB9>
2023-05-04 16:12:34 +0300

    .

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -16,6 +16,6 @@ def generate_question_and_answer():
     step = randint(1, 10)
     start = randint(0, 10)
     progression = get_progression(start, step, length_of_prog)
-    answer = progression.pop(progression[miss_number_index] = '..')
+    answer = progression.pop(progression[miss_number_index] == '..')
     question = " ".join([str(i) for i in progression])
     return question, str(answer)
